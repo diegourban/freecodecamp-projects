93a8062ace6fb7e45feb520874099aea6fc8180a
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-10-05 21:19:43 -0300

    carregando dados

--- Data Visualization/D3/Visualize Data with a Bar Chart/main.js
+++ Data Visualization/D3/Visualize Data with a Bar Chart/main.js
@@ -1,29 +1,36 @@
 var GDPChart = GDPChart || {};
 
 GDPChart.Config = {
-	'url' : 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json',
-	'months' = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-	'formatCurrency' = d3.format("$,.2f");
+  'data_url' : 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json',
+  'months' : ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  'formatCurrency' : d3.format("$,.2f")
 };
 
 GDPChart.BarChart = (function(){
-	var config = GDPChart.Config;
+  var config = GDPChart.Config;
 	
-	var myTimer;
-	var currentSession = config.pomodoroSession;
-	var totalSeconds = 0;
-	var sessionTime = config.pomodoroTime;
-	var minutesLabelId = "minutes";
-	var secondsLabelId = "seconds";
+  var url = config.data_url;
   
-	function drawChart() {
-		// carregar os dados
-		//
-	}
+  function drawChart() {
+    var jqxhr = $.getJSON(url, function() {
+	  console.log( "success" );
+	})
+	.done(function(jsonData) {
+	  console.log("second success");
+	  var data = jsonData.data;
+	  console.log(data);
+	})
+	.fail(function() {
+	  console.log("error");
+	})
+	.always(function() {
+	  console.log("complete");
+	});
+  }
 	
-	return {
-		draw: function() {
-			return drawChart();
-		}
-	};
+  return {
+    draw: function() {
+	  return drawChart();
+	}
+  };
 })();
