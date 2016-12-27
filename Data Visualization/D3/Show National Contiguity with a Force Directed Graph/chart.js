957f4487049094b092c97f0a0238684942dd4260
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-12-27 20:29:41 -0200

    National Contiguity with Force Directed Graph

---
+++ Data Visualization/D3/Show National Contiguity with a Force Directed Graph/chart.js
@@ -0,0 +1,102 @@
+var NationalContiguity = NationalContiguity || {};
+
+NationalContiguity.Config = {
+  'data_url' : 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json'
+};
+
+NationalContiguity.ForceDirectedGraph = (function(){
+  var config = NationalContiguity.Config;
+	
+  var url = config.data_url;
+
+  var simulation;
+  
+  function drawChart() { 
+    var margin = { top: 50, right: 0, bottom: 100, left: 80 }, width = 960, height = 600;
+
+    d3.select(".chart").append("div")
+        .attr("class", "title")
+        .text("Force Directed Graph of State Contiguity");
+
+    var svg = d3.select(".chart").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom);
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    simulation = d3.forceSimulation()
+        .force("link", d3.forceLink().id(function(d) { return d.id; }))
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(width / 2, height / 2));
+
+    d3.json("miserables.json", function(error, graph) {
+      if (error) throw error;
+
+      var link = svg.append("g")
+          .attr("class", "links")
+        .selectAll("line")
+        .data(graph.links)
+        .enter().append("line")
+          .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+      var node = svg.append("g")
+          .attr("class", "nodes")
+        .selectAll("circle")
+        .data(graph.nodes)
+        .enter().append("circle")
+          .attr("r", 5)
+          .attr("fill", function(d) { return color(d.group); })
+          .call(d3.drag()
+              .on("start", dragstarted)
+              .on("drag", dragged)
+              .on("end", dragended));
+
+      node.append("title")
+          .text(function(d) { return d.id; });
+
+      simulation
+          .nodes(graph.nodes)
+          .on("tick", ticked);
+
+      simulation.force("link")
+          .links(graph.links);
+
+      function ticked() {
+        link
+            .attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+
+        node
+            .attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+      }
+    });
+  } 
+
+  function dragstarted(d) {
+      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+  }
+
+  function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+  	
+  return {
+    draw: function() {
+	    return drawChart();
+	  }
+  };
+})();
+
+NationalContiguity.ForceDirectedGraph.draw();
