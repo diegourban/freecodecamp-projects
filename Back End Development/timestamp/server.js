d76882f6516e197f8ac91b70a094120e16dcd351
Diego <diego.urban88@gmail.com>
2017-01-18 21:46:08 +0000

    timestamp microservice

--- Back End Development/timestamp/server.js
+++ Back End Development/timestamp/server.js
@@ -1,10 +1,34 @@
+var strftime = require('strftime')
 var express = require('express')
 var app = express()
 
-app.get('/', function(req, res) {
-    res.send('Hello World!')
+app.get('/:time', function(req, res) {
+    // try to handle as timestamp
+    var date = new Date(parseInt(req.params.time) * 1000)
+    var valid = date.getTime() > 0
+    
+    var unix = null
+    var natural = null
+    
+    if(!valid) {
+        // try to handle as natural language date
+        date = new Date(req.params.time)
+        valid = date.getTime() > 0
+    }
+    
+    if(valid) {
+        unix = date.getTime() / 1000
+        natural = strftime("%B %d, %Y", date)
+    }
+    
+    var timestamp = {
+        'unix' : unix,
+        'natural' : natural
+    }
+    
+    res.send(timestamp)
 })
 
 app.listen(8080, function() {
-    console.log('Example app listening on port 8080!')
+    console.log('Timestamp microservice listening on port 8080!')
 })
